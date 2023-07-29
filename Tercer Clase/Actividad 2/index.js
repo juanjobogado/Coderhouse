0dddea28d3a13395851167a40a2e8d8d1bf42f2c
Juan Bogado <juanjobogado13@gmail.com>
2023-07-29 10:28:50 -0300

    second activity ready

--- Tercer Clase/Actividad 2/index.js
+++ Tercer Clase/Actividad 2/index.js
@@ -1,24 +1,28 @@
-fs = require("fs");
+fs = require("fs").promises;
 
 
 const exercise = async() => {
-    const data = await fs.readFile('package.json', 'utf8');
+    try{
 
-    // Crear un objeto con la información solicitada
-    const info = {
-      contenidoStr: data,
-      contenidoObj: JSON.parse(data),
-      size: Buffer.byteLength(data),
-    };
-
-    // Mostrar el objeto info por consola
-    console.log(info);
-
-    // Guardar el objeto info en un archivo info.json
-    await fs.writeFile('info.json', JSON.stringify(info));
-  } catch (error) {
+        const data = await fs.readFile('Tercer Clase/Actividad 2/package.json', 'utf-8');
+    
+        // Crear un objeto con la información solicitada
+        const info = {
+          contenidoStr: data,
+          contenidoObj: JSON.parse(data),
+          size: Buffer.byteLength(data),
+        };
+    
+        // Mostrar el objeto info por consola
+        console.log(info);
+    
+        // Guardar el objeto info en un archivo info.json
+        await fs.writeFile('info.json', JSON.stringify(info));
+    }
+   catch (error) {
     // Manejar los errores con throw new Error
     throw new Error(error.message);
   }
 
-  exercise();
+}
+exercise();
